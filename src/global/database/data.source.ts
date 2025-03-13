cc5daa2f003f49057f36e67ce59cf3f538b4f047
donggun <hugh3232@naver.com>
2025-03-13 13:35:20 +0900

    chor: .env 파일설정

--- src/global/database/data.source.ts
+++ src/global/database/data.source.ts
@@ -6,12 +6,13 @@ config({ path: 'env.local' });
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST,
   port: 5432,
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: 'aspis_user',
-  entities: [User], // 사용하려는 엔티티 배열
-  synchronize: true, // true면 자동으로 테이블 생성, false면 마이그레이션 사용
-  logging: true, // 로그 출력 여부
+  entities: [User], 
+  synchronize: false, 
+  logging: true, 
+
 });
