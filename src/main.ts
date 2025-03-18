f0e3d9566ecc40bcb68b31b71b07493addca2d8a
0xC0FFE2 <github1@ncloud.sbs>
2025-03-18 11:47:45 +0900

    fix : cors fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule); //AppModule을 사용해 NestJS 애플리케이션을 생성하는 코드야.
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
+  await app.listen(3339);
 }
 bootstrap();
