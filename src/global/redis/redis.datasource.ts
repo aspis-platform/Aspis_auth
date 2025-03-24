a4b00cb91f31c5f5d3468f9db430429b888bb069
0xC0FFE2 <github1@ncloud.sbs>
2025-03-24 09:09:41 +0900

    chore : add redis password

--- src/global/redis/redis.datasource.ts
+++ src/global/redis/redis.datasource.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
 
 @Module({
-  imports: [ConfigModule], 
+  imports: [ConfigModule],
   providers: [
     {
       provide: 'REDIS_CLIENT',
@@ -11,8 +11,9 @@ import Redis from 'ioredis';
         return new Redis({
           host: configService.get<string>('REDIS_HOST'),
           port: configService.get<number>('REDIS_PORT'),
+          password: configService.get<string>('REDIS_PASSWORD', 'password'),
         });
-      },      
+      },
       inject: [ConfigService],
     },
   ],
