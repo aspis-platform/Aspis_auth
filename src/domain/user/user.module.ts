3f5013efb45ea799299675910a8673115ccb13c5
donggun <hugh3232@naver.com>
2025-03-27 22:35:36 +0900

    refactor : 오타 수정

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -12,10 +12,10 @@ import { RefreshToken } from '../auth/dto/entity/refresh.entity';
   imports: [TypeOrmModule.forFeature([User,RefreshToken]),
   RedisModule,
   EmailModule,
-InviteModule],
+  InviteModule], //→ Redis, 이메일 관련 기능, 초대 관련 기능을 사용할 수 있도록 가져옴.
 
-  providers: [UserService,InviteModule],
-  controllers: [UserController],
-  exports: [UserService]
+  providers: [UserService], //이 모듈에서 제공하는 서비스나 의존성 주입을 위한 프로바이더 목록
+  controllers: [UserController],  //이 모듈이 제공하는 API 엔드포인트를 관리하는 컨트롤러 목록.
+  exports: [UserService] //이 모듈이 다른 모듈에서 사용할 수 있도록 내보내는 목록.
 })
 export class UserModule { }
