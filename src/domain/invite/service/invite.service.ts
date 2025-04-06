60a5e9add8778517f6840accddb34e0dfc64511e
donggun <hugh3232@naver.com>
2025-04-06 22:51:02 +0900

    chore:코드 공백 수정

--- src/domain/invite/service/invite.service.ts
+++ src/domain/invite/service/invite.service.ts
@@ -6,21 +6,21 @@ import * as fs from 'fs';
 
 @Injectable()
 export class InviteService {
+  constructor(
+    @Inject('REDIS_CLIENT') private readonly redisClient: Redis,
+    private emailService: EmailService,
+  ) {}
+
   find() {
     throw new Error('Method not implemented.');
   }
-  constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis,
-  private emailService: EmailService
-) {}
 
-async setEmail(email: string, ttl: number = 3600): Promise<string> { //저장된 이메일을 식별할 수 있는 고유한 키를 반환하는 역할을 한다.
+  async setEmail(email: string, ttl: number = 3600): Promise<string> {
     const key = randomUUID();
-    
+
     const filePath = __dirname + '/emailTemplate.html';
     let emailHtml = fs.readFileSync(filePath, 'utf-8');
     emailHtml = emailHtml.replace(/{{key}}/g, key);
-
-    
 
     const emailOptions = {
       to: email,
@@ -28,45 +28,38 @@ async setEmail(email: string, ttl: number = 3600): Promise<string> { //저장된
       text: `안녕하세요! 로그인 인증 키는 https://aspis.ncloud.sbs/join?key=${key}입니다. 이 키를 사용하여 로그인을 진행하세요.`,
       html: emailHtml,
     };
+
     await this.emailService.sendEmail(emailOptions);
-
     await this.redisClient.set(key, email, 'EX', ttl);
-    return key
-  }
 
+    return key;
+  }
 
   async getAllUsers(): Promise<any[]> {
+    const keys = await this.redisClient.keys('*');
+    if (keys.length === 0) return [];
 
-    const keys = await this.redisClient.keys('*'); 
-    
-    if (keys.length === 0) return []; // 키가 없으면 빈 배열 반환
-    
-    const values = await this.redisClient.mget(keys); 
-    
-    // key-value 형태로 변환
-    const keyValuePairs = keys.map((key, index) => ({
-      id: key,
-      email: values[index]
-    })).filter(item => item.email !== null); // null 값 제외
-    
+    const values = await this.redisClient.mget(keys);
+
+    const keyValuePairs = keys
+      .map((key, index) => ({
+        id: key,
+        email: values[index],
+      }))
+      .filter((item) => item.email !== null);
+
     return keyValuePairs;
-  }    
-
-  
-async deleteEmail(key: string): Promise<{ message: string }> {
-  // Redis에서 키로 이메일을 가져옴
-  const user_email = await this.redisClient.get(key);
-
-  // 키에 해당하는 이메일이 존재하지 않으면
-  if (!user_email) {
-      throw new HttpException('KEY_NOT_FOUND', HttpStatus.NOT_FOUND); // '키값이 존재하지 않습니다'
   }
 
-  // 이메일 삭제
-  await this.redisClient.del(key);
+  async deleteEmail(key: string): Promise<{ message: string }> {
+    const user_email = await this.redisClient.get(key);
 
-  // 이메일 삭제 성공 메시지 반환
-  return { message: 'Email deleted successfully' };
-}
+    if (!user_email) {
+      throw new HttpException('KEY_NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
 
+    await this.redisClient.del(key);
+
+    return { message: 'Email deleted successfully' };
+  }
 }
